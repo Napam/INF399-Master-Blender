a3a1be5f50a0e054905525b1d7988b0d37108b04
Naphat Amundsen <naphat@live.no>
2020-09-03 16:14:13 +0200

    Added gloss

--- Blender/Fish/blender_config.py
+++ Blender/Fish/blender_config.py
@@ -6,3 +6,13 @@ TRGT_CLTN = 'Copies'
 SPAWNBOX = 'spawnbox'
 ROT_MUS = [np.pi/2, 0, np.pi]
 ROT_STDS = [0.5, 1, 1]
+
+'''Filesystem'''
+GENERATED_DATA_DIR = 'generated_data'
+# BBOX_FILE = 'bboxes.csv'
+IMAGE_DIR = 'images'
+IMAGE_NAME = 'img'
+
+'''DB'''
+BBOX_DB_FILE = 'bboxes.db'
+BBOX_DB_TABLE = 'bboxes'
