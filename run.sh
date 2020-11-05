72f6b99a5959932065f3b50914a5a0383e16fc33
Naphat Amundsen <naphat@live.no>
2020-11-05 12:09:24 +0100

    Added nogit to gitignore and updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 DOCKER_FLAGS=""
-GPU="all"
 ARGS=""
 
 error() {
@@ -8,19 +7,19 @@ error() {
 }
 
 while getopts "dpg:" option; do
-        case $option in
-                d) DOCKER_FLAGS+=" -d";;
-		g) GPU=${OPTARG};;
-		p) DOCKER_FLAGS+=" --publish 5556:8888";;
-                *) error; exit;;
-        esac
+    case $option in
+        d) DOCKER_FLAGS+="-d ";;
+        g) DOCKER_FLAGS+="--gpus ${OPTARG} ";;
+        p) DOCKER_FLAGS+="--publish 5556:8888 ";;
+        *) error; exit;;
+    esac
 done
 
 # $@ is an array or something, start at $OPTIND and rest
 ARGS+=${@:$OPTIND}
 
 docker run ${DOCKER_FLAGS} -it -v $(pwd)/volume:/app \
-        --ipc=host --rm --gpus ${GPU} --name blender-nam012-cntr nam012-blender ${ARGS}
+        --ipc=host --rm --name blender-nam012-cntr nam012-blender ${ARGS}
 
 
 
